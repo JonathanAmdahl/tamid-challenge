13c7278c5bee027e9ed116b7a40de5840ec51cf2
Jonathan Amdahl <jonamdahl2@gmail.com>
2022-11-15 21:26:43 -0700

    major styling done

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Particles from "../components/particles";
 import styles from "../styles/Home.module.scss";
 
@@ -7,17 +8,27 @@ export default function Home() {
       <div className={styles.background}>
         <Particles />
       </div>
-      <h1>Tamid URL Shortner</h1>
+      <h1>
+        <span>Tamid</span> URL Shortner
+      </h1>
       <p>
-        Create custom short links to easily share with others. Get started for
-        free by pasting your link, clicking shorten, and copying your new link.
+        Create custom short links to easily share with others.
+        <br />
+        Get started for free by pasting your link, clicking shorten,
+        <br />
+        and copying your new link.
       </p>
       <form>
         <input />
         <button>Shorten</button>
       </form>
-      <p>Your new link will appear here after clicking shorten</p>
-      <button>Copy</button>
+      <div className={styles.linkContainer}>
+        <p>Shorten a link to start</p>
+        <button>
+          Copy{" "}
+          <Image src="/copy-icon.svg" width={20} height={20} alt="Copy Text" />
+        </button>
+      </div>
     </div>
   );
 }
